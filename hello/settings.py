25dadd573c9eca16bf3fabfffc80b033d0413ff7
@ProfNandaa <profnandaa@gmail.com>
2015-11-25 23:48:47 +0300

    Switch to MySQL from SQLite

--- hello/settings.py
+++ hello/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'hello.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_polls',
+        'USER': 'prof',
+        'PASSWORD': 'pr0f',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
