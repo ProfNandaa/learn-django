003ac37fef37d46dbdda4acf511641266017f8ba
@ProfNandaa <profnandaa@gmail.com>
2015-12-02 02:14:58 +0300

    Restructured and added AngularJS
    To check out this later -
    http://django-angular.readthedocs.org/en/latest/integration.html

--- hello/settings.py
+++ hello/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'hello.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'public/templates'), os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "public/static"),
+)
 
 # Rest Framework Settings
 REST_FRAMEWORK = {
